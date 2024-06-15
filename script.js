5cf9dfafc0ab2c9c9a90d7a3c9cf735da5bfa6a5
KrishnoSaha <108836045+KrishnoSaha@users.noreply.github.com>
2024-06-15 14:08:13 +0600

    last
    searching different news, opening the articles in new window. it was fun

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const apiKey= '063fa2054bdf46e78ca901aac303cb7b'
 const blogContainer= document.getElementById(blogs);
- async function fetchRandomNews(){
+const searchField= document.getElementById('src_in');
+const searchButton= document.getElementById("button");
+async function fetchRandomNews(){
     try{
        const apiUrl= `https://newsapi.org/v2/everything?q=tesla&pageSize=20&apiKey=${apiKey}`
        const response= await fetch(apiUrl);
@@ -15,6 +17,35 @@ const blogContainer= document.getElementById(blogs);
     }
 
 }
+
+searchButton.addEventListener("click",async ()=>{
+    const query= searchField.value.trim();
+    if(query!== ""){
+        try{
+            const articles= await fetchNewsQuery(query);
+            displayBlogs(articles);
+
+        }catch(error){
+            console.log("error fetching in query", error);
+        }
+    }
+})
+async function fetchNewsQuery(query) {
+    try{
+        const apiUrl= `https://newsapi.org/v2/everything?q=${query}&pageSize=20&apiKey=${apiKey}`;
+        const response= await fetch(apiUrl);
+        const data= await response.json();
+        
+        return data.articles;
+ 
+     }catch(error){
+         console.error("error faching random news", error);
+         return[];
+ 
+     }
+
+}
+
 function displayBlogs(articles){
     blogs.innerHTML= "";
     articles.forEach((articles)=>{
@@ -27,11 +58,15 @@ function displayBlogs(articles){
         const truncatedTitle= articles.title.length>40? articles.title.slice(0, 40)+ ".....": articles.title;
         title.textContent= truncatedTitle;
         const description= document.createElement("p");
-        description.textContent= articles.description;
+        const truncatedDes= articles.description.length > 120? articles.description.slice(0, 120): articles.description;
+        description.textContent= truncatedDes; 
 
         blogCard.appendChild(img);
         blogCard.appendChild(title);
         blogCard.appendChild(description);
+        blogCard.addEventListener('click', ()=>{
+            window.open(articles.url, "_blank");
+        })
         blogs.appendChild(blogCard);
 
 
